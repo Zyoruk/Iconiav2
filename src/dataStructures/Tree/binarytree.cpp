64d057dcd56f41b55da699d4cef638f6249d1806
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-09-11 11:53:30 +1200

    deleted removeAux in order to re-implement

--- src/dataStructures/Tree/binarytree.cpp
+++ src/dataStructures/Tree/binarytree.cpp
@@ -73,43 +73,6 @@ bool BinaryTree::remove(int pElement){
 }
 
 bool BinaryTree::removeAux (int pElement , BinaryTreeNode* pCurrentNode){
-    //if it is a leaf
-    if (pCurrentNode->getLeft() == NULL && pCurrentNode->getRight() == NULL){
-        //is it is the same
-        if (pElement == *(pCurrentNode->getElement())){
-            return true;
-        }else{
-<<<<<<< HEAD
-
-            //if has both leaves
-            // Removing a node that has both left and right.
-            // This would be the fourth case.
-            BinaryTreeNode* _temp = lowestHigher(pleaf);
-            lowestHigher(pleaf)->setElement(*pleaf->getElement());
-            pleaf->setElement(*_temp->getElement());
-            delete _temp;
-            return true;
-
-=======
-            return false;
-        }
-
-    //If the element to remove is higher than the current node and the right is not null
-    }else if (*(pCurrentNode->getElement()) < pElement && pCurrentNode->getRight() != NULL){
-        return removeAux(pElement , pCurrentNode->getRight());
-    //If the element to remove is lower than the current node and the left is not null
-    }else if (*(pCurrentNode->getElement()) > pElement && pCurrentNode->getLeft() != NULL){
-        return removeAux(pElement , pCurrentNode->getLeft());
->>>>>>> 422101285cf8da03feb87db49ffac0c95505f062
-    }else{
-
-        if (pCurrentNode->getLeft() == NULL){
-            pCurrentNode->setElement(*(lowestHighest(pCurrentNode->getRight())->getElement()));
-        }else{
-           // pCurrentNode->setElement(*(highestLowest(pCurrentNode->getLeft())->getElement()));
-        }
-    }
-
 
 }
 

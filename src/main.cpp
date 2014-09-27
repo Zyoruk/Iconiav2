eb1a777f276f6a811beaf354efb4d2e2a790bec7
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-09-28 11:51:31 +1200

    Fixed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
 using namespace std;
-<<<<<<< HEAD
-#include "src/genetico/genetico.h"
-int main()
-{
-    Genetico* temp = new Genetico(30 , 52); //el numero y su cociente deben ser par // el arreglo parcial hace que haya tres hijos cuando se dan 100 elementos
-=======
 #include "src/dataStructures/Graph/graph.h"
 #include "iostream"
 int main()
@@ -14,7 +8,6 @@ int main()
     temp->add(198);
     temp->connect(98,198);
     temp->moveAB(98,198);
->>>>>>> fa4dda088f3d44da7cdeb50fcc8b1426a83c4f5c
     return 0;
 }
 

895316c8eeed4ed0d2c77b383219e0ef2010532c
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-09-28 14:22:21 +1200

    main stuff..................

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 using namespace std;
-<<<<<<< HEAD
-#include "src/genetico/genetico.h"
-int main()
-{
-=======
 #include "src/dataStructures/Graph/graph.h"
 #include "src/dataStructures/SimpleList/SimpleList.h"
 #include "iostream"
@@ -17,12 +12,7 @@ int main()
     temp->connect(198,200);
     temp->connect(200,56);
     temp->connect(98,198);
-<<<<<<< HEAD
-    temp->moveAB(98,198);
->>>>>>> eb1a777f276f6a811beaf354efb4d2e2a790bec7
-=======
     //temp->moveAB(98,198);
->>>>>>> f84636fea4eb9ecccecf985252792bfdbabd0957
     return 0;
 }
 

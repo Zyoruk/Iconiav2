8774d8d52db0d6bad5caa5b5ee66559758c9e5e8
Josav <josav09@gmail.com>
2014-09-28 14:21:56 -0600

    se limpia el main

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,7 @@
 using namespace std;
-<<<<<<< HEAD
-#include "src/genetico/genetico.h"
+
 int main()
 {
-=======
-#include "src/dataStructures/Graph/graph.h"
-#include "src/dataStructures/SimpleList/SimpleList.h"
-#include "iostream"
-int main()
-{
-    Graph<int>* temp = new Graph<int>();
-    temp->add(98);
-    temp->add(198);
-    temp->add(200);
-    temp->add(56);
-    temp->connect(198,200);
-    temp->connect(200,56);
-    temp->connect(98,198);
-<<<<<<< HEAD
-    temp->moveAB(98,198);
->>>>>>> eb1a777f276f6a811beaf354efb4d2e2a790bec7
-=======
-    //temp->moveAB(98,198);
->>>>>>> f84636fea4eb9ecccecf985252792bfdbabd0957
-    return 0;
+return 0;
 }
 

f84636fea4eb9ecccecf985252792bfdbabd0957
Luis Ángel Simón <luisangel1993julio@gmail.com>
2014-09-28 14:09:08 +1200

    Fixedclear method. Working graph.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 using namespace std;
 #include "src/dataStructures/Graph/graph.h"
+#include "src/dataStructures/SimpleList/SimpleList.h"
 #include "iostream"
 int main()
 {
     Graph<int>* temp = new Graph<int>();
     temp->add(98);
     temp->add(198);
+    temp->add(200);
+    temp->add(56);
+    temp->connect(198,200);
+    temp->connect(200,56);
     temp->connect(98,198);
-    temp->moveAB(98,198);
+    //temp->moveAB(98,198);
     return 0;
 }
 
